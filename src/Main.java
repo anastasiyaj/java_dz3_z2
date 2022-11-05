7040773ccf7a25527182e76eef8ae506fee004fc
Anastasiya Kobeleva <anastasiya.kobeleva.95@gmail.com>
2022-11-05 17:42:05 +0300

    reformate code

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ public class Main {
         int bank = 300;
         int refill = 1100;
         int bonus;
-        if (refill>1000) {
-            bonus = refill/100;
+        if (refill > 1000) {
+            bonus = refill / 100;
             bank = bank + refill + bonus;
-            System.out.println("Зачислено бонусов за пополнение счета = "+bonus);
-            System.out.println("Сумма на счету = "+bank);
-        }
-        else{
+            System.out.println("Зачислено бонусов за пополнение счета = " + bonus);
+            System.out.println("Сумма на счету = " + bank);
+        } else {
             bank = bank + refill;
-            System.out.println("Сумма на счету = "+bank);
+            System.out.println("Сумма на счету = " + bank);
         }
-            }
+    }
 }
